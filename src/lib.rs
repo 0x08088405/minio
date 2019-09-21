15d068344b243d70a32e180460d40185016b748a
viri <git@stuff.moe>
2019-09-20 18:48:58 -0600

    improve doc generation in ReadPrimitives

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::{
-    convert::identity, io, mem::size_of, slice, string::FromUtf16Error, string::FromUtf8Error,
+    convert::identity,
+    io,
+    mem::size_of,
+    slice,
+    string::{FromUtf16Error, FromUtf8Error},
 };
 
 macro_rules! read_impl {
     ($t: ty, $name: literal, $fn: ident) => {
         #[inline(always)]
-        #[doc = "Reads a `"] #[doc = $name] #[doc = "` from the underlying reader."]
+        #[doc = "Reads "] #[doc = $name] #[doc = "from the underlying reader."]
         fn $fn(&mut self) -> io::Result<$t> {
             Ok(<$t>::from_ne_bytes(read_impl_body!(self, $t)))
         }
@@ -17,19 +21,19 @@ macro_rules! read_impl {
 
     ($read_t: ty, $map: expr, $ret_t: ty, $name: literal, $le: ident, $be: ident, $ne: ident) => {
         #[inline(always)]
-        #[doc = "Reads a `"] #[doc = $name] #[doc = "` (little-endian) from the underlying reader."]
+        #[doc = "Reads "] #[doc = $name] #[doc = "(little-endian) from the underlying reader."]
         fn $le(&mut self) -> io::Result<$ret_t> {
             Ok($map(<$read_t>::from_le_bytes(read_impl_body!(self, $read_t))))
         }
 
         #[inline(always)]
-        #[doc = "Reads a `"] #[doc = $name] #[doc = "` (big-endian) from the underlying reader."]
+        #[doc = "Reads "] #[doc = $name] #[doc = "(big-endian) from the underlying reader."]
         fn $be(&mut self) -> io::Result<$ret_t> {
             Ok($map(<$read_t>::from_be_bytes(read_impl_body!(self, $read_t))))
         }
 
         #[inline(always)]
-        #[doc = "Reads a `"] #[doc = $name] #[doc = "` (native-endian) from the underlying reader."]
+        #[doc = "Reads "] #[doc = $name] #[doc = "(native-endian) from the underlying reader."]
         fn $ne(&mut self) -> io::Result<$ret_t> {
             Ok($map(<$read_t>::from_ne_bytes(read_impl_body!(self, $read_t))))
         }
@@ -45,18 +49,18 @@ macro_rules! read_impl_body {
 }
 
 pub trait ReadPrimitives: io::Read {
-    read_impl!(i8, "i8", read_i8);
-    read_impl!(u8, "u8", read_u8);
-    read_impl!(i8, "i8", read_i8_le, read_i8_be, read_i8_ne);
-    read_impl!(u8, "u8", read_u8_le, read_u8_be, read_u8_ne);
-    read_impl!(i16, "i16", read_i16_le, read_i16_be, read_i16_ne);
-    read_impl!(u16, "u16", read_u16_le, read_u16_be, read_u16_ne);
-    read_impl!(i32, "i32", read_i32_le, read_i32_be, read_i32_ne);
-    read_impl!(u32, "u32", read_u32_le, read_u32_be, read_u32_ne);
-    read_impl!(i64, "i64", read_i64_le, read_i64_be, read_i64_ne);
-    read_impl!(u64, "u64", read_u64_le, read_u64_be, read_u64_ne);
-    read_impl!(i128, "i128", read_i128_le, read_i128_be, read_i128_ne);
-    read_impl!(u128, "u128", read_u128_le, read_u128_be, read_u128_ne);
+    read_impl!(i8, "an `i8`", read_i8);
+    read_impl!(u8, "a `u8`", read_u8);
+    read_impl!(i8, "an `i8`", read_i8_le, read_i8_be, read_i8_ne);
+    read_impl!(u8, "a `u8`", read_u8_le, read_u8_be, read_u8_ne);
+    read_impl!(i16, "an `i16`", read_i16_le, read_i16_be, read_i16_ne);
+    read_impl!(u16, "a `u16`", read_u16_le, read_u16_be, read_u16_ne);
+    read_impl!(i32, "an `i32`", read_i32_le, read_i32_be, read_i32_ne);
+    read_impl!(u32, "a `u32`", read_u32_le, read_u32_be, read_u32_ne);
+    read_impl!(i64, "an `i64`", read_i64_le, read_i64_be, read_i64_ne);
+    read_impl!(u64, "a `u64`", read_u64_le, read_u64_be, read_u64_ne);
+    read_impl!(i128, "an `i128`", read_i128_le, read_i128_be, read_i128_ne);
+    read_impl!(u128, "a `u128`", read_u128_le, read_u128_be, read_u128_ne);
 
     #[rustfmt::skip]
     read_impl!(u32, |x| f32::from_bits(x), f32, "f32", read_f32_le, read_f32_be, read_f32_ne);
