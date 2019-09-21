fa126918318af04aa590ac4e4a0730d6e2fee66f
viri <git@stuff.moe>
2019-09-20 23:06:40 -0600

    prefix impl macros with _

--- src/lib.rs
+++ src/lib.rs
@@ -6,42 +6,42 @@ use std::{
     string::{FromUtf16Error, FromUtf8Error},
 };
 
-macro_rules! read_impl {
+macro_rules! _read_impl {
     // Used for i8 and u8, as they are endian independent.
     ($t: ty, $name: literal, $fn: ident) => {
         #[inline(always)]
         #[doc = "Reads "] #[doc = $name] #[doc = "from the underlying reader."]
         fn $fn(&mut self) -> io::Result<$t> {
-            Ok(<$t>::from_ne_bytes(read_impl_body!(self, $t)))
+            Ok(<$t>::from_ne_bytes(_read_impl_body!(self, $t)))
         }
     };
 
     ($t: ty, $name: literal, $le: ident, $be: ident, $ne: ident) => {
-        read_impl!($t, identity, $t, $name, $le, $be, $ne);
+        _read_impl!($t, identity, $t, $name, $le, $be, $ne);
     };
 
     ($read_t: ty, $map: expr, $ret_t: ty, $name: literal, $le: ident, $be: ident, $ne: ident) => {
         #[inline(always)]
         #[doc = "Reads "] #[doc = $name] #[doc = "(little-endian) from the underlying reader."]
         fn $le(&mut self) -> io::Result<$ret_t> {
-            Ok($map(<$read_t>::from_le_bytes(read_impl_body!(self, $read_t))))
+            Ok($map(<$read_t>::from_le_bytes(_read_impl_body!(self, $read_t))))
         }
 
         #[inline(always)]
         #[doc = "Reads "] #[doc = $name] #[doc = "(big-endian) from the underlying reader."]
         fn $be(&mut self) -> io::Result<$ret_t> {
-            Ok($map(<$read_t>::from_be_bytes(read_impl_body!(self, $read_t))))
+            Ok($map(<$read_t>::from_be_bytes(_read_impl_body!(self, $read_t))))
         }
 
         #[inline(always)]
         #[doc = "Reads "] #[doc = $name] #[doc = "(native-endian) from the underlying reader."]
         fn $ne(&mut self) -> io::Result<$ret_t> {
-            Ok($map(<$read_t>::from_ne_bytes(read_impl_body!(self, $read_t))))
+            Ok($map(<$read_t>::from_ne_bytes(_read_impl_body!(self, $read_t))))
         }
     };
 }
 
-macro_rules! read_impl_body {
+macro_rules! _read_impl_body {
     ($context: expr, $t: ty) => {{
         let mut buf = [0u8; size_of::<$t>()];
         $context.read_exact(&mut buf)?;
@@ -49,7 +49,7 @@ macro_rules! read_impl_body {
     }};
 }
 
-macro_rules! write_impl {
+macro_rules! _write_impl {
     // Used for i8 and u8, as they are endian independent.
     ($t: ty, $name: literal, $fn: ident) => {
         #[inline(always)]
@@ -60,7 +60,7 @@ macro_rules! write_impl {
     };
 
     ($t: ty, $name: literal, $le: ident, $be: ident, $ne: ident) => {
-        write_impl!($t, identity, $name, $le, $be, $ne);
+        _write_impl!($t, identity, $name, $le, $be, $ne);
     };
 
     ($t: ty, $map: expr, $name: literal, $le: ident, $be: ident, $ne: ident) => {
@@ -91,20 +91,20 @@ macro_rules! write_impl {
 /// (except `isize` and `usize` as their size is platform dependent).
 #[rustfmt::skip]
 pub trait ReadPrimitives: io::Read {
-    read_impl!(i8, "an `i8`", read_i8);
-    read_impl!(u8, "a `u8`", read_u8);
-    read_impl!(i8, "an `i8`", read_i8_le, read_i8_be, read_i8_ne);
-    read_impl!(u8, "a `u8`", read_u8_le, read_u8_be, read_u8_ne);
-    read_impl!(i16, "an `i16`", read_i16_le, read_i16_be, read_i16_ne);
-    read_impl!(u16, "a `u16`", read_u16_le, read_u16_be, read_u16_ne);
-    read_impl!(i32, "an `i32`", read_i32_le, read_i32_be, read_i32_ne);
-    read_impl!(u32, "a `u32`", read_u32_le, read_u32_be, read_u32_ne);
-    read_impl!(i64, "an `i64`", read_i64_le, read_i64_be, read_i64_ne);
-    read_impl!(u64, "a `u64`", read_u64_le, read_u64_be, read_u64_ne);
-    read_impl!(i128, "an `i128`", read_i128_le, read_i128_be, read_i128_ne);
-    read_impl!(u128, "a `u128`", read_u128_le, read_u128_be, read_u128_ne);
-    read_impl!(u32, |x| f32::from_bits(x), f32, "an `f32`", read_f32_le, read_f32_be, read_f32_ne);
-    read_impl!(u64, |x| f64::from_bits(x), f64, "an `f64`", read_f64_le, read_f64_be, read_f64_ne);
+    _read_impl!(i8, "an `i8`", read_i8);
+    _read_impl!(u8, "a `u8`", read_u8);
+    _read_impl!(i8, "an `i8`", read_i8_le, read_i8_be, read_i8_ne);
+    _read_impl!(u8, "a `u8`", read_u8_le, read_u8_be, read_u8_ne);
+    _read_impl!(i16, "an `i16`", read_i16_le, read_i16_be, read_i16_ne);
+    _read_impl!(u16, "a `u16`", read_u16_le, read_u16_be, read_u16_ne);
+    _read_impl!(i32, "an `i32`", read_i32_le, read_i32_be, read_i32_ne);
+    _read_impl!(u32, "a `u32`", read_u32_le, read_u32_be, read_u32_ne);
+    _read_impl!(i64, "an `i64`", read_i64_le, read_i64_be, read_i64_ne);
+    _read_impl!(u64, "a `u64`", read_u64_le, read_u64_be, read_u64_ne);
+    _read_impl!(i128, "an `i128`", read_i128_le, read_i128_be, read_i128_ne);
+    _read_impl!(u128, "a `u128`", read_u128_le, read_u128_be, read_u128_ne);
+    _read_impl!(u32, |x| f32::from_bits(x), f32, "an `f32`", read_f32_le, read_f32_be, read_f32_ne);
+    _read_impl!(u64, |x| f64::from_bits(x), f64, "an `f64`", read_f64_le, read_f64_be, read_f64_ne);
 }
 
 impl<R> ReadPrimitives for R where R: io::Read {}
@@ -115,20 +115,20 @@ impl<R> ReadPrimitives for R where R: io::Read {}
 /// All functions return bytes written, as all `io::Write` functions do.
 #[rustfmt::skip]
 pub trait WritePrimitives: io::Write {    
-    write_impl!(i8, "an `i8`", write_i8);
-    write_impl!(u8, "a `u8`", write_u8);
-    write_impl!(i8, "an `i8`", write_i8_le, write_i8_be, write_i8_ne);
-    write_impl!(u8, "a `u8`", write_u8_le, write_u8_be, write_u8_ne);
-    write_impl!(i16, "an `i16`", write_i16_le, write_i16_be, write_i16_ne);
-    write_impl!(u16, "a `u16`", write_u16_le, write_u16_be, write_u16_ne);
-    write_impl!(i32, "an `i32`", write_i32_le, write_i32_be, write_i32_ne);
-    write_impl!(u32, "a `u32`", write_u32_le, write_u32_be, write_u32_ne);
-    write_impl!(i64, "an `i64`", write_i64_le, write_i64_be, write_i64_ne);
-    write_impl!(u64, "a `u64`", write_u64_le, write_u64_be, write_u64_ne);
-    write_impl!(i128, "an `i128`", write_i128_le, write_i128_be, write_i128_ne);
-    write_impl!(u128, "a `u128`", write_u128_le, write_u128_be, write_u128_ne);
-    write_impl!(f32, |x: f32| x.to_bits(), "an `f32`", write_f32_le, write_f32_be, write_f32_ne);
-    write_impl!(f64, |x: f64| x.to_bits(), "an `f64`", write_f64_le, write_f64_be, write_f64_ne);
+    _write_impl!(i8, "an `i8`", write_i8);
+    _write_impl!(u8, "a `u8`", write_u8);
+    _write_impl!(i8, "an `i8`", write_i8_le, write_i8_be, write_i8_ne);
+    _write_impl!(u8, "a `u8`", write_u8_le, write_u8_be, write_u8_ne);
+    _write_impl!(i16, "an `i16`", write_i16_le, write_i16_be, write_i16_ne);
+    _write_impl!(u16, "a `u16`", write_u16_le, write_u16_be, write_u16_ne);
+    _write_impl!(i32, "an `i32`", write_i32_le, write_i32_be, write_i32_ne);
+    _write_impl!(u32, "a `u32`", write_u32_le, write_u32_be, write_u32_ne);
+    _write_impl!(i64, "an `i64`", write_i64_le, write_i64_be, write_i64_ne);
+    _write_impl!(u64, "a `u64`", write_u64_le, write_u64_be, write_u64_ne);
+    _write_impl!(i128, "an `i128`", write_i128_le, write_i128_be, write_i128_ne);
+    _write_impl!(u128, "a `u128`", write_u128_le, write_u128_be, write_u128_ne);
+    _write_impl!(f32, |x: f32| x.to_bits(), "an `f32`", write_f32_le, write_f32_be, write_f32_ne);
+    _write_impl!(f64, |x: f64| x.to_bits(), "an `f64`", write_f64_le, write_f64_be, write_f64_ne);
 }
 
 impl<W> WritePrimitives for W where W: io::Write {}
